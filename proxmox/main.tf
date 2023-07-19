76ec79ca9d266bd95929b0eebe32d4a139a2e04e
shimosyan <nesher1121@yahoo.co.jp>
2023-07-20 00:49:40 +0900

    terraform test

--- proxmox/main.tf
+++ proxmox/main.tf
@@ -4,10 +4,42 @@
 terraform {
   required_version = ">= 1.0.0"
 
+  backend "s3" {
+    bucket  = "terraform.micmnis.net"
+    region  = "ap-northeast-1"
+    key     = "raspi-cluster/proxmox/terraform.tfstate"
+    encrypt = true
+  }
+
   required_providers {
     proxmox = {
-      source  = "shimosyan/proxmox"
-      version = ">=1.0.0"
+      source  = "telmate/proxmox"
+      version = ">=2.9.14"
     }
   }
 }
+
+provider "proxmox" {
+  pm_api_url = "https://proxmox.micmnis.net/api2/json"
+}
+
+resource "proxmox_lxc" "basic" {
+  target_node  = "pve"
+  hostname     = "lxc-basic"
+  ostemplate   = "synology-nfs:vztmpl/ubuntu-jammy-arm64-default.tar.gz"
+  password     = "BasicLXCContainer"
+  unprivileged = true
+
+  // Terraform will crash without rootfs defined
+  rootfs {
+    storage = "synology-nfs"
+    size    = "8G"
+  }
+
+  network {
+    name   = "eth0"
+    bridge = "vmbr0"
+    ip     = "dhcp"
+  }
+}
+
