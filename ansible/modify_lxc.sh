8648795f8ad1ad268e09a8d6dcf824379f36a94d
shimosyan <nesher1121@yahoo.co.jp>
2023-07-22 20:22:04 +0900

    test ansible script

---
+++ ansible/modify_lxc.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+if [ $# -ne 1 ]; then
+  echo "実行するには1個の引数が必要です。対象の VM ID を指定してください。" 1>&2
+  exit 1
+fi
+
+CONFIG_FILE="/etc/pve/lxc/$1.conf"
+
+if [ ! -f $CONFIG_FILE ]; then
+  echo "ファイルが見つかりません。 $CONFIG_FILE"
+  exit 0;
+fi
+
+echo "ファイルが見つかりました。 $CONFIG_FILE"
