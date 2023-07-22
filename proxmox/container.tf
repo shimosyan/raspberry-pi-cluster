e5d8bd53f822a809c5f29c5828fac3a76cc9083f
shimosyan <nesher1121@yahoo.co.jp>
2023-07-22 17:45:09 +0900

    add github action container

--- proxmox/container.tf
+++ proxmox/container.tf
@@ -1,6 +1,40 @@
-resource "proxmox_lxc" "basic" {
+resource "proxmox_lxc" "github_action_runner_and_ansible" {
+  vmid         = 102
+  hostname     = "github-action-runner-and-ansible"
+  target_node  = "raspi-4gb-2"
+  ostemplate   = var.lxc_os_template
+  arch         = "arm64"
+  ostype       = "ubuntu"
+  unprivileged = false
+  onboot       = true
+  cores        = 1
+  memory       = 512
+  swap         = 0
+  password     = var.root_pw
+  start        = true # インスタンスをスタートしていないと削除できない Ref. https://github.com/Telmate/terraform-provider-proxmox/issues/801
+
+  // rootfs を記述しないとクラッシュするので注意
+  rootfs {
+    storage = "synology-nfs"
+    size    = "8GB"
+  }
+
+  network {
+    name     = "eth0"
+    bridge   = "vmbr0"
+    firewall = true
+    ip       = "192.168.6.67/24"
+    gw       = "192.168.6.1"
+  }
+
+  nameserver = "192.168.2.1"
+}
+
+/*
+resource "proxmox_lxc" "k8s" {
   for_each = local.container
 
+  vmid         = each.value["vmid"]
   hostname     = each.value["hostname"]
   target_node  = each.value["target_node"]
   ostemplate   = var.lxc_os_template
@@ -30,3 +64,5 @@ resource "proxmox_lxc" "basic" {
 
   nameserver = "192.168.2.1"
 }
+
+*/
