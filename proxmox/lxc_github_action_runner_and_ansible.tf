0811037084f3009865c2e97a605bb5389031185b
shimosyan <nesher1121@yahoo.co.jp>
2023-07-23 17:38:09 +0900

    separated file

---
+++ proxmox/lxc_github_action_runner_and_ansible.tf
@@ -0,0 +1,34 @@
+resource "proxmox_lxc" "github_action_runner_and_ansible" {
+  # Enable Switch, 1 = true, 0 = false
+  count = 1
+
+  vmid         = 102
+  hostname     = "github-action-runner-and-ansible"
+  target_node  = "raspi-4gb-2"
+  ostemplate   = var.lxc_os_template
+  arch         = "arm64"
+  ostype       = "ubuntu"
+  unprivileged = false
+  onboot       = true
+  cores        = 1
+  memory       = 512 # 512MB ないと Ansible が動かない
+  swap         = 0
+  password     = var.root_pw
+  start        = true # インスタンスをスタートしていないと削除できない Ref. https://github.com/Telmate/terraform-provider-proxmox/issues/801
+
+  // rootfs を記述しないとクラッシュするので注意
+  rootfs {
+    storage = "synology-nfs"
+    size    = "8G"
+  }
+
+  network {
+    name     = "eth0"
+    bridge   = "vmbr0"
+    firewall = true
+    ip       = "192.168.6.67/24"
+    gw       = "192.168.6.1"
+  }
+
+  nameserver = "192.168.2.1"
+}
