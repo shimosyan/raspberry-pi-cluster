1940aa602ca60ca007ecb1dfd646772258a44c62
shimosyan <nesher1121@yahoo.co.jp>
2023-07-23 18:10:11 +0900

    rm local

--- proxmox/local.tf
+++
@@ -1,9 +0,0 @@
-locals {
-  container_list = yamldecode(file("${path.module}/container.yaml"))
-
-  // container_list から enable が true のものを抽出する
-  container = {
-    for container, value in local.container_list :
-    container => value if value.enable == true
-  }
-}
