7947c20eb7f9e02134d52dccad2844e9a94015b2
shimosyan <nesher1121@yahoo.co.jp>
2023-07-25 01:17:51 +0900

    fix

--- proxmox/variable.tf
+++ proxmox/variable.tf
@@ -2,7 +2,7 @@ variable "root_pw" {
   type = string
 }
 
-variable "lxc_os_template" {
+variable "lxc_os_template_ubuntu" {
   type    = string
   default = "synology-nfs:vztmpl/ubuntu-jammy-arm64-default.tar.xz"
 }
