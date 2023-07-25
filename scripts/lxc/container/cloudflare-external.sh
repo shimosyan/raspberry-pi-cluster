daa383874e611030f4b39f99e7a56550c252d952
shimosyan <nesher1121@yahoo.co.jp>
2023-07-26 01:56:55 +0900

    fix cloudflare external setup script

--- scripts/lxc/container/cloudflare-external.sh
+++ scripts/lxc/container/cloudflare-external.sh
@@ -27,17 +27,50 @@ events {
         # multi_accept on;
 }
 
+# Minecraft サーバーの設定
 stream {
-    server {
-        listen 25565;
-        proxy_pass 192.168.6.69:54621;
-    }
+        upstream mcserver {
+                server 192.168.6.69:54621;
+        }
+
+        server {
+                listen 25565;
+                proxy_pass mcserver;
+        }
 }
 
+# Minecraft ポータル Web サーバーの設定
+http {
+        sendfile on;
+        tcp_nopush on;
+        types_hash_max_size 2048;
+
+        include /etc/nginx/mime.types;
+        default_type application/octet-stream;
+
+        ssl_protocols TLSv1 TLSv1.1 TLSv1.2 TLSv1.3; # Dropping SSLv3, ref: POODLE
+        ssl_prefer_server_ciphers on;
+
+        gzip on;
+
+        server {
+                listen 80;
+                server_name _;
+                location / {
+                        proxy_set_header Host \$http_host;
+                        proxy_pass https://192.168.6.70:8080;
+
+                        proxy_http_version 1.1;
+                        proxy_set_header Connection \$http_connection;
+                        proxy_set_header Origin http://\$host;
+                        proxy_set_header Upgrade \$http_upgrade;
+                }
+        }
+}
 EOF
 
 # nginx を起動
-docker run --name nginx -d -v /root/nginx.conf:/etc/nginx/nginx.conf --restart always -p 80:80 -p 81:81 nginx:latest
+docker run --name nginx -d -v /root/nginx.conf:/etc/nginx/nginx.conf --restart always -p 80:80 -p 25565:25565 nginx:latest
 
 # cloudflared を起動
 docker run --name cloudflare -d --restart always cloudflare/cloudflared:latest tunnel --no-autoupdate run --token $TOKEN
