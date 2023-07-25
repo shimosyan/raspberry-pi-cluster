7a182d64a016602a195c0da3f823012d16dbec39
shimosyan <nesher1121@yahoo.co.jp>
2023-07-26 00:13:05 +0900

    add cloudflare-external

---
+++ scripts/lxc/host/cloudflare-external.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+if [ $# -ne 2 ]; then
+  echo "実行するには2個の引数が必要です。対象の VM ID と CONTAINER SCRIPT NAMEを指定してください。" 1>&2
+  exit 1
+fi
+
+LXC_VM_ID=$1
+LXC_SCRIPT_NAME=$2
+
+CONFIG_FILE="/etc/pve/lxc/$LXC_VM_ID.conf"
+
+#コンテナを起動
+pct start $LXC_VM_ID
+
+# ~/scripts/container に該当するファイルが見つかれば、 LXC に転送して実行する。
+CONTAINER_SCRIPT_FILE="/root/scripts/container/$SCRIPT_NAME.sh"
+
+if [ -e $CONTAINER_SCRIPT_FILE ]; then
+  # セットアップスクリプトを送信
+  pct push $LXC_VM_ID /root/scripts/container/$SCRIPT_NAME.sh /root/setup.sh
+
+  pct exec $LXC_VM_ID chmod +x /root/setup.sh
+
+  # この cloudflare-external.sh ではスクリプトを起動しない
+  #pct exec $LXC_VM_ID /root/setup.sh
+fi
