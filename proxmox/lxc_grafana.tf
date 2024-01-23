e474fe5e2205ae78b93105e359255d1219164281
shimosyan <nesher1121@yahoo.co.jp>
2024-01-23 23:53:56 +0900

    add grafana

---
+++ proxmox/lxc_grafana.tf
@@ -0,0 +1,34 @@
+resource "proxmox_lxc" "grafana" {
+  # Enable Switch, 1 = true, 0 = false
+  count = 1
+
+  vmid         = 107
+  hostname     = "grafana"
+  target_node  = "raspi-8gb-2"
+  ostemplate   = var.lxc_os_template_ubuntu
+  arch         = "arm64"
+  ostype       = "ubuntu"
+  unprivileged = false # NFS のマウントで必要
+  onboot       = true
+  cores        = 1
+  memory       = 512
+  swap         = 0
+  password     = var.root_pw
+  start        = true # インスタンスをスタートしていないと削除できない Ref. https://github.com/Telmate/terraform-provider-proxmox/issues/801
+
+  // rootfs を記述しないとクラッシュするので注意
+  rootfs {
+    storage = "synology-nfs"
+    size    = "64G"
+  }
+
+  network {
+    name     = "eth0"
+    bridge   = "vmbr0"
+    firewall = true
+    ip       = "192.168.6.72/24"
+    gw       = "192.168.6.1"
+  }
+
+  nameserver = "192.168.2.1"
+}
---
+++ scripts/lxc/container/grafana.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+cd `dirname $0`
+
+FILE="/root/.setup_done"
+
+# このスクリプトが一度でも実行されたログがあるなら処理を継続しない
+if [ -e $FILE ]; then
+  echo "Setup Done File exists."
+  exit 0;
+fi
+
+apt update
+
+# Docker のインストール
+apt install -y apt-transport-https ca-certificates curl gnupg lsb-release
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+apt update
+apt install -y docker-ce docker-ce-cli containerd.io docker-compose-plugin
+
+docker run -d --name=grafana --restart always -p 3000:3000 grafana/grafana-enterprise:10.0.0
