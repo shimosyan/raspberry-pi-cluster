bc8329569dc4fd8a58d15e82b0028d4fb8cab394
shimosyan <nesher1121@yahoo.co.jp>
2024-02-11 19:46:38 +0900

    grafana instance ram up

--- proxmox/lxc_grafana.tf
+++ proxmox/lxc_grafana.tf
@@ -11,7 +11,7 @@ resource "proxmox_lxc" "grafana" {
   unprivileged = false # NFS のマウントで必要
   onboot       = true
   cores        = 1
-  memory       = 512
+  memory       = 1024
   swap         = 0
   password     = var.root_pw
   start        = true # インスタンスをスタートしていないと削除できない Ref. https://github.com/Telmate/terraform-provider-proxmox/issues/801
